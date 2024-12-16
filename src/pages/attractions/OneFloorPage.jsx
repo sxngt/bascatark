b4d7a4dd0e4bf5f987265c693e35c9ec03348d9d
sxngt <sxngt@icloud.com>
2024-12-16 09:32:41 +0900

    Vite용 상대겅로 테스트

--- src/pages/attractions/OneFloorPage.jsx
+++ src/pages/attractions/OneFloorPage.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import MainNavigation from "../../components/MainNavigation.jsx";
+import image1 from '/src/assets/slides/1f/1.png';
+import image2 from '/src/assets/slides/1f/2.png';
+import image3 from '/src/assets/slides/1f/3.png';
+import image4 from '/src/assets/slides/1f/4.png';
+import image5 from '/src/assets/slides/1f/5.png';
 
 const OneFloorPage = () => {
 	// 실제로는 각각 다른 이미지 경로가 들어갈 자리입니다
-	const images = [
-		'/src/assets/slides/1f/1.png',
-		'/src/assets/slides/1f/2.png',
-		'/src/assets/slides/1f/3.png',
-		'/src/assets/slides/1f/4.png',
-		'/src/assets/slides/1f/5.png'
-	];
+	const images = [image1, image2, image3, image4, image5];
 
 	return (
 		<div className="fixed inset-0 w-screen h-screen bg-white overflow-y-auto">
